022cefe6008d858aec8f40134d37f9ace349387b
Aravind <aravindsiva2003.in@gmail.com>
2024-08-09 13:49:48 +0530

    sample navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-
-import img from "@/assets/logo.svg";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
-      <Image src={img} alt="logo" className="w-96" />
-      <h1 className="text-4xl text-white">Coming Soon!</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
